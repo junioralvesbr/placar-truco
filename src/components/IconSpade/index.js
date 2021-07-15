ef52ee663f2388d14d5cbac7192953a765fde76e
Junior Alves <jrnalves@gmail.com>
2021-07-14 22:02:57 -0300

    create IconSpade component

---
+++ src/components/IconSpade/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import image from '../../assets/spade.png'
+
+const IconSpade = styled.img.attrs({
+  src: image,
+  alt: "icone de espada do baralho"
+})`
+  width: 50px;
+  height: auto;
+`
+
+export default IconSpade
