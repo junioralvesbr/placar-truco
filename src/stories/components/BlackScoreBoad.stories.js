2be8cdcb12abbceb57cfac4c6faa6dc548672485
Junior Alves <jrnalves@gmail.com>
2021-07-14 22:04:07 -0300

    create iconSpade and BlackScoreBoard storie

---
+++ src/stories/components/BlackScoreBoad.stories.js
@@ -0,0 +1,13 @@
+import BlackScoreBoard from '../../components/BlackScoreBoard'
+
+export default {
+  title: 'Components/BlackScoreBoard',
+  component: BlackScoreBoard,
+  parameters: {
+    layout: 'centered'
+  }
+}
+
+const Template = () => <BlackScoreBoard />
+
+export const Default = Template.bind({})
