dc52e7849e7d05f543ae6905f040cba1db0b9797
Junior Alves <jrnalves@gmail.com>
2021-07-17 12:58:38 -0300

    add args to cardPoint

--- src/stories/components/CardPoint.stories.js
+++ src/stories/components/CardPoint.stories.js
@@ -8,6 +8,16 @@ export default {
   }
 }
 
-const Template = () => <CardPoint />
+const Template = args => <CardPoint {...args} />
 
-export const Default = Template.bind({})
+export const Spade = Template.bind({})
+Spade.args = {
+  card: 'spade',
+  point: '3'
+}
+
+export const Heart = Template.bind({})
+Heart.args = {
+  card: 'heart',
+  point: '3'
+}
