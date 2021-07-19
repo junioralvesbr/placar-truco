69d248ea1753e644b24de2681ab9047aaa7b94aa
Junior Alves <jrnalves@gmail.com>
2021-07-19 15:56:24 -0300

    add title in storybook

--- src/stories/components/ScoreBoard.stories.js
+++ src/stories/components/ScoreBoard.stories.js
@@ -12,12 +12,14 @@ const Template = args => <ScoreBoard {...args} />
 
 export const Spade = Template.bind({})
 Spade.args = {
+  title: 'NÓS',
   card: 'spade',
   score: '0'
 }
 
 export const Heart = Template.bind({})
 Heart.args = {
+  title: "ELES",
   card: 'heart',
   score: '0'
 }
